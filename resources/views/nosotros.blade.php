e15c0fa653179655be881021d0e0ba898a9825b9
informaticarie <informaticarie@gmail.com>
2022-10-18 15:18:33 -0500

    se actualizo el apartado de nosotros en un 95%

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -5,116 +5,120 @@
 @section('contenido')
 
     <div class="container">
+
+        <br>
+        <br>
         <div class="row ">
 
-            <div class="row gx-5 align-items-center my-5" style="text-align: justify;">
-                <div class="col-5"><img class="img-fluid rounded mb-0" src="{{ asset('img/quien.png') }}" alt="..."></div>
-                <div class="col-7 contornoPrin">
-                    <h1 class="font-weight-light">¿Quiénes somos?</h1>
-                    <p> Laboratorio ProMédica surge en el año 2015 en Pachuca de Soto Hgo. Con el objetivo de ayudar al
-                        diagnóstico oportuno y preventivo de la salud, a través de una amplia gama de estudios de
-                        laboratorio, realizados por profesionales especializados en su área. Contando con estándares
-                        internos y externos de calidad los cuales permiten garantizar la precisión y confiabilidad en los
-                        resultados. </p>
-                    <p> En <b>Laboratorio Promédica</b>, generamos calidad en atención y servicio. </p>
-                    <p> Ponemos a su disposición el laboratorio central en el interior del <b>Centro Médico Radiológico
-                            Piracantos</b>. Ubicado en <b>Av. Solidaridad No. 218 Col. Piracantos. Pachuca Hgo. Tels. 771
-                            158 55 14 ó 771 165 04 24</b>, en esta unidad también contamos con los servicios de
-                        <b>Ultrasonido y Electrocardiograma</b> que con agrado le atenderemos.
-                    </p>
-
-                </div>
-            </div>
-        </div>
-
-
-        <div class="row contornoPrin">
-            <div class="col-4" style="margin-bottom: 12vw;">
-                <span style="padding: 2vw;">
-                    <img src="img/Botones/mision.png" class="img-fluid" style="width: 80%; ">
-                </span>
-
-                <p class="probandoBorde"><b> Somos un laboratorio de análisis clínicos comprometidos con la calidad
-                        técnica y humana en constante
-                        crecimiento, aportando resultados confiables para la prevención y diagnóstico de la salud, logrando
-                        así la satisfacción de las necesidades de nuestros usuarios.</b>
-                </p>
-
-
-            </div>
-            <div class="col-4">
-                <br>
-                <br>
-                <img class="img-fluid rounded mb-0" src="{{ asset('img/mivifle.png') }}" alt="...">
-
-            </div>
-            <div class="col-4" style="margin-top: 12vw">
-                <span style="padding: 2vw;">
-                    <img src="img/Botones/vision.png" class="img-fluid" style="width: 80%;">
-                </span>
-
-
-                <p class="probandoBorde"><b> Ser el mejor grupo de apoyo al diagnóstico para pacientes y médicos; y líderes
-                        en fomentar la cultura de la prevención al cuidado de la salud, para mejorar la calidad de vida en
-                        generaciones presentes y futuras, con la excelencia y calidad que nos distingue.</b>
-                </p>
-
-            </div>
-
-        </div>
-
-
-
-        <div class="row ">
-            <div class="row gx-3 align-items-center my-5">
-                <div class="col-10 contornoPrin">
-
-                    <h1 class="font-weight-light">NUESTROS VALORES</h1>
-
-                    <div style="margin-top: 2vw" class="row ">
-                        <div class="col-4">
-                            <ul>
-
-                                <li><i style="color: #303066"
-                                        class="fa-solid fa-heart-circle-check fa-2xl"></i>&nbsp;&nbsp;Amor</li>
-                                <li style="margin-top: 2vw"><i style="color: #303066"
-                                        class="fa-solid fa-scale-balanced fa-2xl"></i>&nbsp;&nbsp;Ética</li>
-                                <li style="margin-top: 2vw"><i style="color: #303066"
-                                        class="fa-solid fa-people-group fa-2xl"></i>&nbsp;&nbsp;Compromiso</li>
-
-                            </ul>
-                        </div>
-                        <div class="col-4">
-                            <ul>
-                                <li><i style="color: #303066"
-                                        class="fa-solid fa-person-digging fa-2xl"></i>&nbsp;&nbsp;Resolución</li>
-                                <li style="margin-top: 2vw"><i style="color: #303066"
-                                        class="fa-solid fa-hand-holding-medical fa-2xl"></i>Pasión</li>
-                                <li style="margin-top: 2vw"><i style="color: #303066"
-                                        class="fa-solid fa-people-carry-box fa-2xl"></i>&nbsp;&nbsp;Responsabilidad</li>
-
-                            </ul>
-                        </div>
-                        <div class="col-4">
-                            <ul>
-                                <li><i style="color: #303066"
-                                        class="fa-solid fa-user-clock fa-2xl"></i>&nbsp;&nbsp;Puntualidad</li>
-                                <li style="margin-top: 2vw"><i style="color: #303066"
-                                        class="fa-solid fa-handshake-angle fa-2xl"></i>&nbsp;&nbsp;Claridad</li>
-                            </ul>
+            <div id="fondo" class="contenedor " >
+                <div class="texto-encima">
+                    <div class="row">
+                        <div class="col-7">
+                            <label  style="padding: 2vw; " align="left" >
+                                <img src="img/quiensomos.png" style="width: 75%; " class="img-fluid">
+                            </label>
+                            <br>
+                            <label class="probandoBorde" style="text-align: justify!important;">
+                                <b>Laboratorio ProMédica</b> surge en el año 2015 en Pachuca de Soto, Hidalgo.
+                                Con el objetivo de ayudar al diagnostico oportuno y preventivo de la salud,
+                                a través de una <b>amplia gama de estudios de laboratorio</b>, realizados por
+                                profesionales especializados en su área. Contando con estandares internos
+                                y externos de calidad los cuales permiten garantizar la precisión y
+                                <b>confiabilidad en los resultados</b>.
+                            </label>
                         </div>
 
 
                     </div>
 
-
-
                 </div>
-
-
 
             </div>
         </div>
+        <br>
+        <br>
+
+
+        <div class="row contornoPrin">
+
+            <div class="col-2" align="center">
+                <span style="padding: 2vw;">
+                    <img src="img/Icon2.png" class="img-fluid" style="width: 50%; ">
+                </span>
+            </div>
+
+            <div class="col-10">
+                <p class="probandoBorde" style="text-align: justify!important;">En <b>Laboratorio ProMédica</b>, generamos
+                    calidad en atención y servicio. Ponemos a su disposición
+                    el laboratorio central en el interior del Centro Médico Radiologico Piracantos. Ubicado en Av.
+                    Solidaridad
+                    No. 218 Col. Piracantos. Pachuca Hgo. Tels. 771 158 55 14 y 771 165 04 24, en esta unidad también
+                    contamos con los servicios de Ultrasonido y Electrocardiograma que con agrado le atenderemos.
+                </p>
+
+            </div>
+
+
+        </div>
+        <br>
+        <br>
+
+
+        <div class="row ">
+            <div class="col-12" align="right">
+
+                <img class="img-fluid" src="{{ asset('img/Banner_3_Nosotros.png') }}" alt="...">
+
+            </div>
+        </div>
+        <br>
+        <br>
+        <div class="row contornoPrin">
+            <div class="col-5">
+                <span class="center" style="padding: 2vw;">
+                    <img src="img/Botones/mision.png" class="img-fluid">
+                </span>
+
+                <p class="probandoBorde" style="text-align: center!important;"> Somos un laboratorio de análisis clínicos
+                    comprometidos con la calidad
+                    técnica y humana en constante
+                    crecimiento, aportando resultados confiables para la prevención y diagnóstico de la salud, logrando
+                    así la satisfacción de las necesidades de nuestros usuarios.
+                </p>
+
+
+            </div>
+            <div class="col-2">
+                <br>
+                <br>
+                <img class="img-fluid center" src="{{ asset('img/Icon4.png') }}" style="width: 80%; " alt="...">
+
+            </div>
+
+            <div class="col-5">
+                <span class="center" style="padding: 2vw;">
+                    <img src="img/Botones/vision.png" class="img-fluid">
+                </span>
+
+
+                <p class="probandoBorde" style="text-align: center!important;">Ser el mejor grupo de apoyo al diagnóstico
+                    para pacientes y médicos; y líderes
+                    en fomentar la cultura de la prevención al cuidado de la salud, para mejorar la calidad de vida en
+                    generaciones presentes y futuras, con la excelencia y calidad que nos distingue.
+                </p>
+
+            </div>
+
+        </div>
+
+
+
+
+
+
+
+
+
     </div>
 
 
